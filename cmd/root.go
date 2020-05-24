f240b2009d27760935c2ce50c3c52ca5128545f5
sergey.cybenko <cybenko.sergey@gmail.com>
2020-05-24 10:38:26 +0300

    Add ReadDir()

--- cmd/root.go
+++ cmd/root.go
@@ -18,17 +18,15 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"go-envdir/pkg"
 	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "go-envdir",
+	Use:   "goenvdir",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -36,9 +34,15 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	DisableFlagParsing: true,
+	Args:               cobra.MinimumNArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("ROOT CMD")
+		err := pkg.Run(args)
+		if err != nil {
+			fmt.Println(err)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -51,8 +55,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -62,30 +64,4 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".go-envdir" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".go-envdir")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
---
+++ pkg/core.go
@@ -0,0 +1,91 @@
+package pkg
+
+import (
+	"bufio"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+// Main execution line
+func Run(args []string) error {
+	dir, err := filepath.Abs(args[0])
+	if err != nil {
+		return err
+	}
+
+	prog := args[1]
+	if len(args) > 2 {
+		args = args[2:]
+	}
+
+	envSlice, err := ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	RunCmd(prog, args, envSlice)
+
+	return nil
+}
+
+// Read dir and extract env variables from all *.env files
+func ReadDir(dir string) (envSlice []string, err error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return
+	}
+
+	var envs []string
+	for _, file := range files {
+		if path.Ext(file.Name()) == ".env" {
+			envs, err = readFile(path.Join(dir, file.Name()))
+			if err != nil {
+				return
+			}
+			envSlice = append(envSlice, envs...)
+		}
+	}
+
+	return
+}
+
+// Run command with env variables
+func RunCmd(prog string, args []string, env []string) int {
+	cmd := exec.Command(prog, args...)
+	cmd.Env = env
+	return 1
+}
+
+// Read env file
+func readFile(filename string) (envSlice []string, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	return Parse(file)
+}
+
+// Extract all env variables from file
+func Parse(r io.Reader) (envSlice []string, err error) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		trimmedLine := strings.TrimSpace(scanner.Text())
+		if len(trimmedLine) == 0 || strings.HasPrefix(trimmedLine, "#") {
+			continue
+		}
+		envSlice = append(envSlice, scanner.Text())
+	}
+
+	if err = scanner.Err(); err != nil {
+		return
+	}
+
+	return
+}
